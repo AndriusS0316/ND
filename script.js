51970e8fe64f0142e245f8bd99d6032641bc0959
Andrius <andriuss0316@gmail.com>
2021-08-04 22:41:26 +0300

    20210804

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+
+const pateikti = document.querySelector(".pateikti");
+
+const vardas = document.querySelector("#vardas");
+const pavarde = document.querySelector("#pavarde");
+const gimimoMatai = document.querySelector("#gimimoMatai");
+const gimimoMenesis = document.querySelector("#gimimoMenesis");
+const gimimoDiena = document.querySelector("#gimimoDiena");
+const alga = document.querySelector("#alga");
+
 function parodytiZmones() {
     const list = getZmones();
     const div = document.getElementById("sarasas");
@@ -7,4 +18,15 @@ function parodytiZmones() {
     }
     str += "</ul>";
     div.innerHTML = str;
+};
+
+function pateiktiClick() {
+    vardas.value = '';
+    pavarde.value = '';
+    gimimoMatai.value = '';
+    gimimoMenesis.value = '';
+    gimimoDiena.value = '';
+    alga.value = '';
 }
+
+pateikti.addEventListener('click', pateiktiClick);
