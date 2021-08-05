72858da5275a5a8e039cc96c7940609240e8111d
Andrius <andriuss0316@gmail.com>
2021-08-05 10:13:32 +0300

    20210805

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 const pateikti = document.querySelector(".pateikti");
+const delete1 = document.querySelector(".delete");
 
 const vardas = document.querySelector("#vardas");
 const pavarde = document.querySelector("#pavarde");
@@ -8,6 +9,14 @@ const gimimoMatai = document.querySelector("#gimimoMatai");
 const gimimoMenesis = document.querySelector("#gimimoMenesis");
 const gimimoDiena = document.querySelector("#gimimoDiena");
 const alga = document.querySelector("#alga");
+
+// 1 <= gimimoMenesis.value && gimimoMenesis.value <= 12;
+
+// if (gimimoMenesis.value === 2) {
+//     1 <= gimimoDiena.value && gimimoDiena.value <= 29;
+// } else {
+//     1 <= gimimoDiena.value && gimimoDiena.value <= 31;
+// };
 
 
 
@@ -23,15 +32,15 @@ function parodytiZmones() {
 };
 
 function pateiktiClick() {
-    addZmogus(zmogus) {
-        zmogus {
-            zmogus.vardas = vardas.value,
-            zmogus.pavarde = pavarde.value,
-            zmogus.gimimoData = gimimoMatai.value + "-" + gimimoMenesis.value + "-" + gimimoDiena.value,
-            zmogus.alga = alga.value
-        }
-        
-    }
+    zmones.push({
+        id: nextId.value,
+        vardas: vardas.value,
+        pavarde: pavarde.value,
+        gimimoData: gimimoMatai.value + "-" + gimimoMenesis.value + "-" + gimimoDiena.value,
+        alga: alga.value  
+    })
+
+    nextId += 1;
 
     vardas.value = '';
     pavarde.value = '';
@@ -41,4 +50,9 @@ function pateiktiClick() {
     alga.value = '';
 }
 
+function IstrintiClick() {
+
+}
+
 pateikti.addEventListener('click', pateiktiClick);
+delete1.addEventListener('click', IstrintiClick);
