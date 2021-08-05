88ab0a39fdd6ea5d841dcdcb036587307d7384c1
Andrius <andriuss0316@gmail.com>
2021-08-05 15:00:17 +0300

    20210805

--- script.js
+++ script.js
@@ -31,6 +31,59 @@ function parodytiZmones() {
     div.innerHTML = str;
 };
 
+function addZmogusForma() {
+    const div = document.getElementById("sarasas");
+    div.nextSibling.remove
+    div.append(document.createTextNode("Vardas:"));
+    div.append(document.createElement("br"));
+    let inputElement = document.createElement("input");
+    inputElement.id = "vardas";
+    div.append(inputElement);
+    div.append(document.createTextNode("Pavarde:"));
+    let inputElement = document.createElement("input");
+    inputElement.id = "pavarde";
+    div.append(inputElement);
+    div.append(document.createElement("br"));
+    div.append(document.createTextNode("Gimimo diena:"));
+    let inputElement = document.createElement("input");
+    inputElement.id = "gimimoDiena";
+    inputElement.type = "date";
+    div.append(inputElement);
+    div.append(document.createElement("br"));
+    div.append(document.createTextNode("Alga:"));
+    let inputElement = document.createElement("input");
+    inputElement.id = "alga";
+    inputElement.type = "number";
+    div.append(inputElement);
+    div.append(document.createElement("br"));
+    let saveButton = document.createElement("button");
+    saveButton.append(document.createTextNode("Save"));
+    div.append(saveButton);
+    let saveButton = document.createElement("button");
+    cancelButton.append(document.createTextNode("Cancel"));
+    cancelButton.onclick = parodytiZmones;
+    div.append(cancelButton);
+}
+
+function saveZmogus() {
+    const vardas = document.getElementById("vardas").value;
+    const pavarde = document.getElementById("pavarde").value;
+    const gimimoData = document.getElementById("gimimoDiena").value;
+    const alga = document.getElementById("alga").value;
+    const naujasZmogus = {
+        
+    }
+}
+
+function clearNode(node) {
+    while (node.firstElementChild) {
+        node.nfirstElementChild.remove();
+    }
+}
+
+
+
+
 function pateiktiClick() {
     zmones.push({
         id: nextId.value,
